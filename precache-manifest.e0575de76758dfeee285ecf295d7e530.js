1f7148cb8e7a5baecc51f140689a5699ab829151
xumick <mickey.xu@schindler.com>
2025-01-02 16:18:07 +0800

    Updates

---
+++ precache-manifest.e0575de76758dfeee285ecf295d7e530.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6b587eeec086d72ede53f6cf556a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04d67581bb13415d56f",
+    "url": "/static/css/main.331231ac.chunk.css"
+  },
+  {
+    "revision": "1ed166e8aaba80405eec",
+    "url": "/static/js/2.bcae1afa.chunk.js"
+  },
+  {
+    "revision": "34e164e173a6e948586ab6bc4d4cc12b",
+    "url": "/static/js/2.bcae1afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04d67581bb13415d56f",
+    "url": "/static/js/main.ba12f9b9.chunk.js"
+  },
+  {
+    "revision": "49db20f848c0fc8ba98d",
+    "url": "/static/js/runtime-main.d40d6ef4.js"
+  },
+  {
+    "revision": "ea78ca4748b61adfbdd5bda462351f99",
+    "url": "/static/media/Elevator.ea78ca47.svg"
+  },
+  {
+    "revision": "5d05d6fd471ea53bf4c6976804bd4239",
+    "url": "/static/media/Favicon.5d05d6fd.png"
+  },
+  {
+    "revision": "3f69b977bc1efe2615a502afbaee029a",
+    "url": "/static/media/about.3f69b977.svg"
+  },
+  {
+    "revision": "e863cca7e666cd020a4b68b59271be39",
+    "url": "/static/media/checkList.e863cca7.svg"
+  },
+  {
+    "revision": "707b29cc1b4e2863723e1f9f65a4ebab",
+    "url": "/static/media/downLoad.707b29cc.svg"
+  },
+  {
+    "revision": "bc99d9db1d5e8b788625ea064be94ad4",
+    "url": "/static/media/fitter.bc99d9db.svg"
+  },
+  {
+    "revision": "7dfa807b256aa8f96b283512e4476c27",
+    "url": "/static/media/fitter2.7dfa807b.svg"
+  },
+  {
+    "revision": "a2da12604c28eb6be6fa262750ebc1a6",
+    "url": "/static/media/fitter3.a2da1260.svg"
+  },
+  {
+    "revision": "411e2c9072de895ada6c7961ca9bb158",
+    "url": "/static/media/key.411e2c90.svg"
+  },
+  {
+    "revision": "231164abbf948805b4e3e92b0f3e5d58",
+    "url": "/static/media/language.231164ab.svg"
+  },
+  {
+    "revision": "8fbb15dd7fe8db681d11c6ee2b6a5736",
+    "url": "/static/media/logo.8fbb15dd.png"
+  },
+  {
+    "revision": "654468a47bcbacd48329d436b7a8a688",
+    "url": "/static/media/nodeStatus.654468a4.png"
+  }
+]);
